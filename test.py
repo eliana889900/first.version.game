ca8f4300baa7a31f1d806a352664dee7e2a89333
eliana889900 <167455464+eliana889900@users.noreply.github.com>
2024-04-18 18:38:00 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-hi tis a test
+from random import randint
+
+print("")
+print("Hi Rina, Hi Marieeeee can yall see this collaborative document?")
+
+print("NOOOO")
+
+print("ITS FINE COZ TTPD IS COMING OUT TWRRRR")
+
+
+print("Fan favourite track on ttpd will be track number", randint(1, 16) )
